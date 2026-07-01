a92db8d68e76a77be5017347fd2b192e86201665
Sabari Gopalsamy <sabari.gopalsamy@gmail.com>
2026-07-01 22:19:57 +0530

    Configure H2 database for CI tests

--- src/test/java/com/sailpoint/lab/hrms_api/HrmsApiApplicationTests.java
+++ src/test/java/com/sailpoint/lab/hrms_api/HrmsApiApplicationTests.java
@@ -2,12 +2,14 @@ package com.sailpoint.lab.hrms_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HrmsApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
